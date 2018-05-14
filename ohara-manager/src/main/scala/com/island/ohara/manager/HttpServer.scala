170e04c9cecb4673ee047be7c598870ac36d3340
Chia-Ping Tsai <chia7712@is-land.com.tw>
2018-05-14 08:13:34 +0000

    Minor: refactor for HttpServer (pull request #31)
    Approved-by: vitojeng <vito@is-land.com.tw>

--- ohara-manager/src/main/scala/com/island/ohara/manager/HttpServer.scala
+++ ohara-manager/src/main/scala/com/island/ohara/manager/HttpServer.scala
@@ -1,21 +1,25 @@
 package com.island.ohara.manager
 
 import java.io.File
-import java.nio.file.{Files, Path, Paths}
 
-import akka.event.{LogSource, Logging}
 import akka.actor.{ActorRef, ActorSystem}
+import akka.event.{LogSource, Logging}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server.directives._
-import ContentTypeResolver.Default
-import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.directives.ContentTypeResolver.Default
+import akka.stream.ActorMaterializer
 import com.island.ohara.manager.sample.{UserRegistryActor, UserRoutes}
 
 import scala.io.StdIn
 
 object HttpServer extends UserRoutes {
-  val ENV_WEBROOT = "ohara.manager.webRoot"
+
+  /**
+    * This relative path is used for testing. For example, you can create the folder src/web in testing and then HttpServer will load it even if
+    * no env variable is configured. You should, however, assign the absolute path by env variable in the production.
+    */
+  val PROP_WEBROOT_DEFAULT = "src/web"
+  val PROP_WEBROOT = "ohara.manager.webRoot"
 
   implicit val system = ActorSystem("ohara-manager")
   val userRegistryActor: ActorRef =
@@ -23,43 +27,38 @@ object HttpServer extends UserRoutes {
   implicit val executionContext = system.dispatcher
   implicit val logSource: LogSource[AnyRef] = new LogSource[AnyRef] {
     def genString(o: AnyRef): String = o.getClass.getName
+
     override def getClazz(o: AnyRef): Class[_] = o.getClass
   }
 
   override lazy val log = Logging(system, this)
 
-  private def srcWebRoot: Option[Path] = {
-    val srcPath = Paths.get(new File("src/web").getCanonicalPath)
-    Some(srcPath).filter(Files.exists(_))
-  }
-
-  private def sysWebRoot: Either[String, Path] =
-    sys.props(ENV_WEBROOT) match {
-      case null => Left(s"$ENV_WEBROOT not exist")
-      case value =>
-        Right(Paths.get(value))
-          .filterOrElse(Files.exists(_), s"$ENV_WEBROOT is `$value`. But this folder is not exist.")
+  /**
+    * Try to locate the folder of web root. First, we lookup the env variable if user have configured a extra folder.
+    * Second, we use the passed local folder if user doesn't config the env variable. If it fails to locate the web root, a exception will be thrown.
+    * NOTED: make it package-private for testing
+    * @param local local path
+    */
+  private[manager] def webRoot(local: String): File = {
+    Option(sys.props(PROP_WEBROOT)).map(new File(_)).filter(_.exists()).getOrElse(new File(local)) match {
+      case f: File if f.exists() => f
+      case _ =>
+        throw new RuntimeException(s"Fail to find the web root. Please set the $PROP_WEBROOT or check the $local")
     }
-
-  private def webRoot: Either[String, Path] = sysWebRoot match {
-    case Left(msg) => srcWebRoot.toRight(msg)
-    case result    => result
   }
 
   def main(args: Array[String]): Unit = {
     implicit val materializer = ActorMaterializer()
 
     try {
-      val _webRoot: File = webRoot match {
-        case Left(msg)   => throw new RuntimeException("Error: " + msg)
-        case Right(path) => path.toFile
-      }
-      log.info(s"Ohara-manager web root: " + _webRoot.toString)
+      val webRoot: File = this.webRoot(PROP_WEBROOT_DEFAULT)
+
+      log.info(s"Ohara-manager web root: " + webRoot.toString)
 
       val route =
         pathPrefix(Segments) { names =>
           get {
-            getFromDirectory(new File(_webRoot, names.mkString("/")).getCanonicalPath)
+            getFromDirectory(new File(webRoot, names.mkString("/")).getCanonicalPath)
           }
         } ~ userRoutes
 
