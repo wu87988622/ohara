2a618fc3981cf9bcfe87179d8c2d2484f5adbfa1
Joshua <joshua@is-land.com.tw>
2018-07-24 02:45:15 +0000

    OHARA-250 run all the tests at the ohara-manager root. (pull request #117)
    * OHARA-250 Add npm scripts for running all tests on the Server
    
    * OHARA-250 Add junit report for the server
    
    * OHARA-250 Remove demo tests in cypress
    
    * OHARA-250 Add cypress-eslint-cypress to remove eslint warning from vscode
    
    * OHARA-250 Change cypress:all to cypress:ci for clearity
    
    * OHARA-250 Change testResults to testReports so it aligns with reset of the services
    
    * OHARA-250 Ignore Ohara manager coverage reports and fix coverage errors
    
    Approved-by: Jack Yang <jack@is-land.com.tw>

--- ohara-manager-nodejs/client/cypress/integration/Home.js
+++ ohara-manager-nodejs/client/cypress/integration/Home.js
@@ -1,7 +1,6 @@
 describe('Home', () => {
   it('loads', () => {
     cy.visit('/');
-    cy.get('h1').should('contain', 'Welcome to React');
-    cy.get('button').should('contain', 'Hello there!');
+    cy.location('pathname').should('eq', '/');
   });
 });
--- ohara-manager-nodejs/client/cypress/integration/TopicPage.js
+++
@@ -1,6 +0,0 @@
-describe('TopicPage', () => {
-  it('loads', () => {
-    cy.visit('/topics');
-    cy.get('[data-test]').should('contain', 'See the topics here!');
-  });
-});
