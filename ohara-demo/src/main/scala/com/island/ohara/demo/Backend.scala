00276b270742b534085d160567b38ce4562d5a7e
Chia-Ping Tsai <chia7712@is-land.com.tw>
2018-08-14 02:59:38 +0000

    OHARA-344 runBackend should accept -Pport and -Pttl (pull request #150)
    Approved-by: Joshua <joshua@is-land.com.tw>

--- ohara-demo/src/main/scala/com/island/ohara/demo/Backend.scala
+++ ohara-demo/src/main/scala/com/island/ohara/demo/Backend.scala
@@ -8,12 +8,30 @@ import com.island.ohara.io.CloseOnce.doClose
 import com.island.ohara.kafka.KafkaClient
 import com.island.ohara.client.ConnectorClient
 import com.island.ohara.serialization.Serializer
+import scala.concurrent.duration._
 
 /**
   * run a configurator based on 3 brokers and 3 workers.
   */
 object Backend {
+  val HELP_KEY = "--help"
+  val PORT_KEY = "--port"
+  val TTL_KEY = "--ttl"
+  val USAGE = s"[Usage] $TTL_KEY $PORT_KEY"
   def main(args: Array[String]): Unit = {
+    if (args.length == 1 && args(0).equals(HELP_KEY)) {
+      println(USAGE)
+      return
+    }
+    if (args.size < 2 || args.size % 2 != 0) throw new IllegalArgumentException(USAGE)
+    // TODO: make the parse more friendly
+    var ttl: Duration = 9999 millis
+    var port: Int = 0
+    args.sliding(2, 2).foreach {
+      case Array(PORT_KEY, value) => port = value.toInt
+      case Array(TTL_KEY, value)  => ttl = value.toInt seconds
+      case _                      => throw new IllegalArgumentException(USAGE)
+    }
     doClose(OharaTestUtil.localWorkers(3, 3)) { util =>
       println("wait for the mini kafka cluster")
       TimeUnit.SECONDS.sleep(5)
@@ -31,12 +49,13 @@ object Backend {
         .kafkaClient(KafkaClient(util.brokers))
         .connectClient(ConnectorClient(util.workers))
         .hostname("localhost")
-        .port(0)
+        .port(port)
         .build()
       try {
         println(s"run a configurator at ${configurator.hostname}:${configurator.port} with topic:$topicName")
-        println(s"enter ctrl+c to terminate all process")
-        TimeUnit.SECONDS.sleep(9999)
+        println(
+          s"enter ctrl+c to terminate all processes (or all processes will be terminated after ${ttl.toSeconds} seconds")
+        TimeUnit.SECONDS.sleep(ttl.toSeconds)
       } finally configurator.close()
     }
   }
