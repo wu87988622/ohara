73bf4fd016c84b1f29371c560ffd54bddb09237d
Joshua <joshua@is-land.com.tw>
2018-09-11 21:10:02 +0000

    Minor: Use env files to remove the clutter in npm scripts and simplify the module import path (pull request #218)

--- ohara-manager/client/src/components/pages/ConfigurationPage.js
+++ ohara-manager/client/src/components/pages/ConfigurationPage.js
@@ -4,13 +4,13 @@ import toastr from 'toastr';
 import DocumentTitle from 'react-document-title';
 import { Prompt } from 'react-router-dom';
 
-import { AppWrapper } from '../common/Layout';
-import { Input, Button, FormGroup, Label } from '../common/Form';
-import { primaryBtn, cancelBtn } from '../../theme/btnTheme';
-import { lighterGray } from '../../theme/variables';
-import { CONFIGURATION } from '../../constants/documentTitles';
-import * as _ from '../../utils/helpers';
-import * as MESSAGES from '../../constants/messages';
+import { AppWrapper } from 'common/Layout';
+import { Input, Button, FormGroup, Label } from 'common/Form';
+import { primaryBtn, cancelBtn } from 'theme/btnTheme';
+import { lighterGray } from 'theme/variables';
+import { CONFIGURATION } from 'constants/documentTitles';
+import * as _ from 'utils/helpers';
+import * as MESSAGES from 'constants/messages';
 import {
   validateHdfs,
   saveHdfs,
