af5bef9ab3c441ca445ee304a38938764a035a05
蔡嘉平 <chia7712@is-land.com.tw>
2018-10-05 06:39:26 +0000

    Minor: make all connectors be instaniable (pull request #322)

--- ohara-kafka/src/test/scala/com/island/ohara/kafka/connector/TestColumnInSource.scala
+++ ohara-kafka/src/test/scala/com/island/ohara/kafka/connector/TestColumnInSource.scala
@@ -2,6 +2,7 @@ package com.island.ohara.kafka.connector
 import java.util
 
 import com.island.ohara.client.ConfiguratorJson.Column
+import com.island.ohara.io.VersionUtil
 import com.island.ohara.rule.SmallTest
 import com.island.ohara.serialization.DataType
 import org.junit.Test
@@ -9,19 +10,9 @@ import org.scalatest.Matchers
 
 class TestColumnInSource extends SmallTest with Matchers {
 
-  private[this] val columns = Seq(Column("cf0", DataType.BOOLEAN, 0), Column("cf1", DataType.BOOLEAN, 1))
   @Test
   def testSource(): Unit = {
-    val source = new RowSourceConnector {
-      override protected def _taskClass(): Class[_ <: RowSourceTask] = ???
-      override protected def _taskConfigs(maxTasks: Int): Seq[TaskConfig] = {
-        Seq(TaskConfig("test", Seq("topic"), columns, Map(Column.COLUMN_KEY -> Column.toString(columns))))
-      }
-      override protected def _start(config: TaskConfig): Unit = ???
-      override protected def _stop(): Unit = ???
-
-      override protected def _version: String = ???
-    }
+    val source = new DumbSource
     // lack column string
     an[IllegalArgumentException] should be thrownBy source.start(new util.HashMap[String, String])
     // It is invalid to assign the columns manually
@@ -30,17 +21,29 @@ class TestColumnInSource extends SmallTest with Matchers {
 
   @Test
   def testTask(): Unit = {
-    val task = new RowSourceTask {
-
-      override protected def _start(config: TaskConfig): Unit = ???
-
-      override protected def _stop(): Unit = ???
-
-      override protected def _poll(): Seq[RowSourceRecord] = ???
-
-      override protected def _version: String = ???
-    }
-
+    val task = new DumbSourceTask
     an[IllegalArgumentException] should be thrownBy task.start(new util.HashMap[String, String])
   }
+}
+
+class DumbSource extends RowSourceConnector {
+  private[this] val columns = Seq(Column("cf0", DataType.BOOLEAN, 0), Column("cf1", DataType.BOOLEAN, 1))
+  override protected def _taskClass(): Class[_ <: RowSourceTask] = classOf[DumbSourceTask]
+  override protected def _taskConfigs(maxTasks: Int): Seq[TaskConfig] = {
+    Seq(TaskConfig("test", Seq("topic"), columns, Map(Column.COLUMN_KEY -> Column.toString(columns))))
+  }
+  override protected def _start(config: TaskConfig): Unit = {}
+  override protected def _stop(): Unit = {}
+
+  override protected def _version: String = VersionUtil.VERSION
+}
+
+class DumbSourceTask extends RowSourceTask {
+  override protected def _start(config: TaskConfig): Unit = {}
+
+  override protected def _stop(): Unit = {}
+
+  override protected def _poll(): Seq[RowSourceRecord] = Seq.empty
+
+  override protected def _version: String = VersionUtil.VERSION
 }
