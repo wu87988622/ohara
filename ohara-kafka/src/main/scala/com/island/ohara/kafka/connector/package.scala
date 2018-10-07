80191410de98fcb4c9d832649ba37c7543c356b5
蔡嘉平 <chia7712@is-land.com.tw>
2018-10-07 14:14:19 +0000

    OHARA-588 Don't remove internal key from connector config (pull request #327)

--- ohara-kafka/src/main/scala/com/island/ohara/kafka/connector/package.scala
+++ ohara-kafka/src/main/scala/com/island/ohara/kafka/connector/package.scala
@@ -4,28 +4,33 @@ import java.util
 import com.island.ohara.client.ConfiguratorJson.Column
 import scala.collection.JavaConverters._
 package object connector {
-  val TOPICS_KEY: String = "topics"
+  private[this] val NAME_KEY: String = "name"
+  private[this] val TOPICS_KEY: String = "topics"
   private[connector] def toTaskConfig(props: util.Map[String, String]): TaskConfig = {
     val options = props.asScala
-    val schema = options.remove(Column.COLUMN_KEY).map(Column.toColumns).getOrElse(Seq.empty)
+    // TODO: the passed props is not a "copy" so any changes to props will impact props itself.
+    // see OHARA-588 for more details...by chia
+    val schema = options.get(Column.COLUMN_KEY).map(Column.toColumns).getOrElse(Seq.empty)
     val topics = options
-      .remove("topics")
+      .get(TOPICS_KEY)
       .map(_.split(","))
       .getOrElse(throw new IllegalArgumentException(s"topics doesn't exist!!!"))
-    val name = options.remove("name").getOrElse(throw new IllegalArgumentException(s"name doesn't exist!!!"))
+    val name = options.remove(NAME_KEY).getOrElse(throw new IllegalArgumentException(s"name doesn't exist!!!"))
     TaskConfig(name, topics, schema, options.toMap)
   }
 
   private[connector] def toMap(taskConfig: TaskConfig): util.Map[String, String] = {
-    if (taskConfig.options.contains(Column.COLUMN_KEY))
-      throw new IllegalArgumentException(s"DON'T touch ${Column.COLUMN_KEY} manually")
-    if (taskConfig.options.contains(TOPICS_KEY))
-      throw new IllegalArgumentException(s"DON'T touch $TOPICS_KEY manually in row connector")
-    if (taskConfig.options.contains("name"))
-      throw new IllegalArgumentException("DON'T touch \"name\" manually in row connector")
+    // TODO: the passed props is not a "copy" so any changes to props will impact props itself.
+    // see OHARA-588 for more details...by chia
+//    if (taskConfig.options.contains(Column.COLUMN_KEY))
+//      throw new IllegalArgumentException(s"DON'T touch ${Column.COLUMN_KEY} manually")
+//    if (taskConfig.options.contains(TOPICS_KEY))
+//      throw new IllegalArgumentException(s"DON'T touch $TOPICS_KEY manually in row connector")
+//    if (taskConfig.options.contains("name"))
+//      throw new IllegalArgumentException("DON'T touch \"name\" manually in row connector")
     if (taskConfig.topics.isEmpty) throw new IllegalArgumentException("empty topics is invalid")
     (taskConfig.options + (Column.COLUMN_KEY -> Column.toString(taskConfig.schema))
       + (TOPICS_KEY -> taskConfig.topics.mkString(","))
-      + ("name" -> taskConfig.name)).asJava
+      + (NAME_KEY -> taskConfig.name)).asJava
   }
 }
---
+++ ohara-kafka/src/test/scala/com/island/ohara/kafka/connector/TestConnectorProps.scala
@@ -0,0 +1,84 @@
+package com.island.ohara.kafka.connector
+import java.util
+
+import com.island.ohara.client.ConfiguratorJson.Column
+import com.island.ohara.io.VersionUtil
+import com.island.ohara.rule.SmallTest
+import com.island.ohara.serialization.DataType
+import org.junit.Test
+import org.scalatest.Matchers
+
+class TestConnectorProps extends SmallTest with Matchers {
+
+  @Test
+  def testNoTopicsInSourceConnector(): Unit = {
+    val connector = new DumbSource
+    // lack topics string
+    an[IllegalArgumentException] should be thrownBy connector.start(new util.HashMap[String, String])
+  }
+
+  @Test
+  def testNoTopicsInSinkConnector(): Unit = {
+    val connector = new DumbSink
+    // lack topics string
+    an[IllegalArgumentException] should be thrownBy connector.start(new util.HashMap[String, String])
+  }
+
+  @Test
+  def testNoTopicsInSourceTask(): Unit = {
+    val task = new DumbSourceTask
+    an[IllegalArgumentException] should be thrownBy task.start(new util.HashMap[String, String])
+  }
+
+  @Test
+  def testNoTopicsInSinkTask(): Unit = {
+    val task = new DumbSinkTask
+    an[IllegalArgumentException] should be thrownBy task.start(new util.HashMap[String, String])
+  }
+
+  // TODO: add tests against adding interval key manually...see OHARA-588
+}
+
+class DumbSource extends RowSourceConnector {
+  private[this] val columns = Seq(Column("cf0", DataType.BOOLEAN, 0), Column("cf1", DataType.BOOLEAN, 1))
+  override protected def _taskClass(): Class[_ <: RowSourceTask] = classOf[DumbSourceTask]
+  override protected def _taskConfigs(maxTasks: Int): Seq[TaskConfig] = {
+    Seq(TaskConfig("test", Seq("topic"), columns, Map(Column.COLUMN_KEY -> Column.toString(columns))))
+  }
+  override protected def _start(config: TaskConfig): Unit = {}
+  override protected def _stop(): Unit = {}
+
+  override protected def _version: String = VersionUtil.VERSION
+}
+
+class DumbSourceTask extends RowSourceTask {
+  override protected def _start(config: TaskConfig): Unit = {}
+
+  override protected def _stop(): Unit = {}
+
+  override protected def _poll(): Seq[RowSourceRecord] = Seq.empty
+
+  override protected def _version: String = VersionUtil.VERSION
+}
+
+class DumbSink extends RowSinkConnector {
+  private[this] val columns = Seq(Column("cf0", DataType.BOOLEAN, 0), Column("cf1", DataType.BOOLEAN, 1))
+  override protected def _taskClass(): Class[_ <: RowSinkTask] = classOf[DumbSinkTask]
+  override protected def _taskConfigs(maxTasks: Int): Seq[TaskConfig] = {
+    Seq(TaskConfig("test", Seq("topic"), columns, Map(Column.COLUMN_KEY -> Column.toString(columns))))
+  }
+  override protected def _start(config: TaskConfig): Unit = {}
+  override protected def _stop(): Unit = {}
+
+  override protected def _version: String = VersionUtil.VERSION
+}
+
+class DumbSinkTask extends RowSinkTask {
+  override protected def _start(config: TaskConfig): Unit = {}
+
+  override protected def _stop(): Unit = {}
+
+  override protected def _version: String = VersionUtil.VERSION
+
+  override protected def _put(records: Seq[RowSinkRecord]): Unit = {}
+}
