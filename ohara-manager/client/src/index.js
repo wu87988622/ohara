591ff0e0c1989bf0404a3cf2bab80167b2adb64d
Joshua <joshua@is-land.com.tw>
2018-10-17 01:05:06 +0000

    OHARA-651 Prevent duplicated notifications from toastr (pull request #370)

--- ohara-manager/client/src/index.js
+++ ohara-manager/client/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from 'App';
 
 import 'theme/globalStyle';
+import 'toastrConfigs';
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ ohara-manager/client/src/toastrConfigs.js
@@ -0,0 +1,4 @@
+import toastr from 'toastr';
+
+// Global configs for toastr
+toastr.options.preventDuplicates = true;
