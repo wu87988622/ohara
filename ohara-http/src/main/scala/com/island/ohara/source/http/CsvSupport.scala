6ecc469939c5e20169892f54959ef0f2cad71ecc
Chia-Ping Tsai <chia7712@is-land.com.tw>
2018-11-05 03:52:44 +0000

    OHARA-619 remove unused code from 0.1 (pull request #473)

--- ohara-http/src/main/scala/com/island/ohara/source/http/CsvSupport.scala
+++
@@ -1,8 +0,0 @@
-package com.island.ohara.source.http
-
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import spray.json.{DefaultJsonProtocol, RootJsonFormat}
-
-trait CsvSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val csvFormat: RootJsonFormat[CSV] = jsonFormat1(CSV)
-}
--- ohara-http/src/main/scala/com/island/ohara/source/http/RowSchema.scala
+++
@@ -1,9 +0,0 @@
-package com.island.ohara.source.http
-
-import com.island.ohara.serialization.DataType
-
-/**
-  * (Cell#name, CellType)
-  * @param schema
-  */
-final case class RowSchema(schema: Vector[(String, DataType)])
