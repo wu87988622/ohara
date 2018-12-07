c51d030e66693f523b30f32158acc637031ee9df
Chia-Ping Tsai <chia7712@is-land.com.tw>
2018-12-07 01:58:38 +0000

    OHARA-939 Fix NPE in failed tests (pull request #554)
    Approved-by: Jack Yang <jack@is-land.com.tw>

--- ohara-testing-util/src/test/java/com/island/ohara/integration/With3Brokers3Workers.java
+++ ohara-testing-util/src/test/java/com/island/ohara/integration/With3Brokers3Workers.java
@@ -1,18 +1,26 @@
 package com.island.ohara.integration;
 
 import com.island.ohara.common.rule.LargeTest;
+import com.island.ohara.common.util.CloseOnce;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 /**
  * This class create a mini broker/worker cluster with 3 nodes. And the cluster will be closed after
  * all test cases have been done.
+ *
+ * <p>NOTED: All subclass has "same" reference to util. This is ok in junit test since it default
+ * run each test on "different" jvm. The "same" static member won't cause trouble in testing.
+ * However, you should move the static "util" into your test if you don't depend on junit...by chia
  */
 public abstract class With3Brokers3Workers extends LargeTest {
   protected static OharaTestUtil util;
 
   @BeforeClass
   public static void beforeAll() {
+    if (util != null)
+      throw new IllegalArgumentException(
+          "The test util had been initialized!!! This happens on your tests don't run on different jvm");
     util = OharaTestUtil.workers();
   }
 
@@ -21,7 +29,9 @@ public abstract class With3Brokers3Workers extends LargeTest {
   }
 
   @AfterClass
-  public static void afterAll() throws Exception {
-    util.close();
+  public static void afterAll() {
+    CloseOnce.close(util);
+    // we have to assign null to util since we allow junit to reuse jvm
+    util = null;
   }
 }
