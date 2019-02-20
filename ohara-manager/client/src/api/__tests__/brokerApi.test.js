ea820543d257b6ed68f6da11b301f06efc38a3f3
Joshua <tank158r5155@yahoo.com.tw>
2019-02-20 09:47:44 +0800

    OHARA-1559 Add tests for client broker and zookeeper APIs (#213)

---
+++ ohara-manager/client/src/api/__tests__/brokerApi.test.js
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2019 is-land
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { fetchBrokers, createBroker } from '../brokerApi';
+import { handleError, axiosInstance } from 'utils/apiUtils';
+
+jest.mock('utils/apiUtils');
+
+const url = '/api/brokers';
+
+describe('fetchZookeeper()', () => {
+  afterEach(jest.clearAllMocks);
+
+  it('handles success http call', async () => {
+    const res = {
+      data: {
+        isSuccess: true,
+      },
+    };
+
+    axiosInstance.get.mockImplementation(() => Promise.resolve(res));
+
+    const result = await fetchBrokers();
+    expect(axiosInstance.get).toHaveBeenCalledTimes(1);
+    expect(axiosInstance.get).toHaveBeenCalledWith(url);
+    expect(result).toBe(res);
+  });
+
+  it('handles success http call but with server error', async () => {
+    const res = {
+      data: {
+        isSuccess: false,
+      },
+    };
+    axiosInstance.get.mockImplementation(() => Promise.resolve(res));
+
+    const result = await fetchBrokers();
+
+    expect(axiosInstance.get).toHaveBeenCalledTimes(1);
+    expect(axiosInstance.get).toHaveBeenCalledWith(url);
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledWith(result);
+  });
+
+  it('handles failed http call', async () => {
+    const res = {
+      data: {
+        errorMessage: {
+          message: 'error!',
+        },
+      },
+    };
+
+    axiosInstance.get.mockImplementation(() => Promise.reject(res));
+
+    await fetchBrokers();
+    expect(axiosInstance.get).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledWith(res);
+  });
+});
+
+describe('createZookeeper()', () => {
+  afterEach(jest.clearAllMocks);
+
+  const params = {
+    name: 'abc',
+    zookeeperClusterName: ['a', 'b'],
+    nodeNames: ['c', 'd'],
+  };
+  const config = { timeout: 180000 };
+
+  it('handles success http call', async () => {
+    const res = {
+      data: {
+        isSuccess: true,
+      },
+    };
+
+    axiosInstance.post.mockImplementation(() => Promise.resolve(res));
+
+    const result = await createBroker(params);
+    expect(axiosInstance.post).toHaveBeenCalledTimes(1);
+    expect(axiosInstance.post).toHaveBeenCalledWith(url, params, config);
+    expect(result).toBe(res);
+  });
+
+  it('handles success http call but with server error', async () => {
+    const res = {
+      data: {
+        isSuccess: false,
+      },
+    };
+    axiosInstance.post.mockImplementation(() => Promise.resolve(res));
+
+    const result = await createBroker(params);
+
+    expect(axiosInstance.post).toHaveBeenCalledTimes(1);
+    expect(axiosInstance.post).toHaveBeenCalledWith(url, params, config);
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledWith(result);
+  });
+
+  it('handles failed http call', async () => {
+    const res = {
+      data: {
+        errorMessage: {
+          message: 'error!',
+        },
+      },
+    };
+
+    axiosInstance.post.mockImplementation(() => Promise.reject(res));
+
+    await createBroker(params);
+    expect(axiosInstance.post).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledWith(res);
+  });
+});
---
+++ ohara-manager/client/src/api/__tests__/zookeeperApi.test.js
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2019 is-land
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { fetchZookeepers, createZookeeper } from '../zookeeperApi';
+import { handleError, axiosInstance } from 'utils/apiUtils';
+
+jest.mock('utils/apiUtils');
+
+const url = '/api/zookeepers';
+
+describe('fetchZookeeper()', () => {
+  afterEach(jest.clearAllMocks);
+  it('handles success http call', async () => {
+    const res = {
+      data: {
+        isSuccess: true,
+      },
+    };
+
+    axiosInstance.get.mockImplementation(() => Promise.resolve(res));
+
+    const result = await fetchZookeepers();
+    expect(axiosInstance.get).toHaveBeenCalledTimes(1);
+    expect(axiosInstance.get).toHaveBeenCalledWith(url);
+    expect(result).toBe(res);
+  });
+
+  it('handles success http call but with server error', async () => {
+    const res = {
+      data: {
+        isSuccess: false,
+      },
+    };
+    axiosInstance.get.mockImplementation(() => Promise.resolve(res));
+
+    const result = await fetchZookeepers();
+
+    expect(axiosInstance.get).toHaveBeenCalledTimes(1);
+    expect(axiosInstance.get).toHaveBeenCalledWith(url);
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledWith(result);
+  });
+
+  it('handles failed http call', async () => {
+    const res = {
+      data: {
+        errorMessage: {
+          message: 'error!',
+        },
+      },
+    };
+
+    axiosInstance.get.mockImplementation(() => Promise.reject(res));
+
+    await fetchZookeepers();
+    expect(axiosInstance.get).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledWith(res);
+  });
+});
+
+describe('createZookeeper()', () => {
+  afterEach(jest.clearAllMocks);
+
+  const params = {
+    name: 'abc',
+    nodeNames: ['a', 'b'],
+  };
+  const config = { timeout: 180000 };
+
+  it('handles success http call', async () => {
+    const res = {
+      data: {
+        isSuccess: true,
+      },
+    };
+
+    axiosInstance.post.mockImplementation(() => Promise.resolve(res));
+
+    const result = await createZookeeper(params);
+    expect(axiosInstance.post).toHaveBeenCalledTimes(1);
+    expect(axiosInstance.post).toHaveBeenCalledWith(url, params, config);
+    expect(result).toBe(res);
+  });
+
+  it('handles success http call but with server error', async () => {
+    const res = {
+      data: {
+        isSuccess: false,
+      },
+    };
+    axiosInstance.post.mockImplementation(() => Promise.resolve(res));
+
+    const result = await createZookeeper(params);
+
+    expect(axiosInstance.post).toHaveBeenCalledTimes(1);
+    expect(axiosInstance.post).toHaveBeenCalledWith(url, params, config);
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledWith(result);
+  });
+
+  it('handles failed http call', async () => {
+    const res = {
+      data: {
+        errorMessage: {
+          message: 'error!',
+        },
+      },
+    };
+
+    axiosInstance.post.mockImplementation(() => Promise.reject(res));
+
+    await createZookeeper(params);
+    expect(axiosInstance.post).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledTimes(1);
+    expect(handleError).toHaveBeenCalledWith(res);
+  });
+});
