73287ad000762cfa38ee70daf38463ee1414506e
Chia-Ping Tsai <chia7712@gmail.com>
2019-03-06 10:23:18 +0800

    OHARA-1644 Validation of FTP should check the permission of input account (#369)

---
+++ ohara-configurator/src/test/scala/com/island/ohara/configurator/endpoint/TestValidationOfRdb.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 is-land
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.island.ohara.configurator.endpoint
+
+import com.island.ohara.client.configurator.v0.ValidationApi.{RdbValidationRequest, ValidationReport}
+import com.island.ohara.client.kafka.{TopicAdmin, WorkerClient}
+import com.island.ohara.common.util.Releasable
+import com.island.ohara.testing.With3Brokers3Workers
+import org.junit.{After, Before, Test}
+import org.scalatest.Matchers
+
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+class TestValidationOfRdb extends With3Brokers3Workers with Matchers {
+  private[this] val taskCount = 3
+  private[this] val topicAdmin = TopicAdmin(testUtil.brokersConnProps)
+  private[this] val rdb = testUtil.dataBase
+  private[this] val workerClient = WorkerClient(testUtil.workersConnProps)
+
+  @Before
+  def setup(): Unit =
+    Await.result(workerClient.plugins(), 10 seconds).exists(_.className == classOf[Validator].getName) shouldBe true
+
+  private[this] def result[T](f: Future[T]): T = Await.result(f, 60 seconds)
+
+  private[this] def assertSuccess(f: Future[Seq[ValidationReport]]): Unit = {
+    val reports = result(f)
+    reports.size shouldBe taskCount
+    reports.isEmpty shouldBe false
+    reports.foreach(_.pass shouldBe true)
+  }
+
+  @Test
+  def goodCase(): Unit = {
+    assertSuccess(
+      Validator.run(
+        workerClient,
+        topicAdmin,
+        RdbValidationRequest(url = rdb.url, user = rdb.user, password = rdb.password, workerClusterName = None),
+        taskCount
+      ))
+  }
+
+  @After
+  def tearDown(): Unit = Releasable.close(topicAdmin)
+}
