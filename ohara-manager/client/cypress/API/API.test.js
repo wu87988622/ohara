bbded9480bca26439a09df8086584ba8d0eda4e1
wu87988622 <wu87988622@gmail.com>
2019-06-11 16:28:57 +0800

    finish pipeline page api test

--- ohara-manager/client/cypress/API/API.test.js
+++ ohara-manager/client/cypress/API/API.test.js
@@ -18,6 +18,12 @@ import { makeRandomPort } from '../utils';
 
 let brokerClusterName = '';
 let jarID = '';
+let pipelineID = '';
+let pipelineName = '';
+let topicID = '';
+let streamAppID = '';
+let connectorID = '';
+let propertyID = '';
 const wkName = `wk${makeRandomPort()}`;
 
 describe('Zookeeper Api test', () => {
@@ -150,6 +156,7 @@ describe('Topic Api test', () => {
     };
     cy.testCreateTopic(data).then(res => {
       const data = res.data;
+      topicID = data.result.id;
       expect(data.isSuccess).to.eq(true);
       expect(data.result).to.include.keys(
         'id',
@@ -307,6 +314,437 @@ describe('Node Api test', () => {
       expect(data.result[0].services[1].clusterNames).to.be.a('array');
       expect(data.result[0].services[2].name).to.be.a('string');
       expect(data.result[0].services[2].clusterNames).to.be.a('array');
+    });
+  });
+});
+
+describe('Pipelines Api test', () => {
+  it('createPipeline', () => {
+    const data = {
+      name: 'Untitled pipeline',
+      rules: {},
+      workerClusterName: wkName,
+    };
+    cy.testCreatePipeline(data).then(res => {
+      const data = res.data;
+      pipelineID = data.result.id;
+      pipelineName = data.result.name;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys(
+        'id',
+        'name',
+        'workerClusterName',
+        'objects',
+      );
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.name).to.be.a('string');
+      expect(data.result.workerClusterName).to.be.a('string');
+      expect(data.result.objects).to.be.a('array');
+    });
+  });
+  it('fetchPipeline', () => {
+    cy.fetchPipeline(pipelineID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys(
+        'id',
+        'name',
+        'workerClusterName',
+        'objects',
+      );
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.name).to.be.a('string');
+      expect(data.result.workerClusterName).to.be.a('string');
+      expect(data.result.objects).to.be.a('array');
+    });
+  });
+  it('fetchPipelines', () => {
+    cy.fetchPipelines().then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.be.a('array');
+      expect(data.result[0]).to.include.keys(
+        'id',
+        'name',
+        'workerClusterName',
+        'objects',
+      );
+      expect(data.result[0].id).to.be.a('string');
+      expect(data.result[0].name).to.be.a('string');
+      expect(data.result[0].workerClusterName).to.be.a('string');
+      expect(data.result[0].objects).to.be.a('array');
+    });
+  });
+  it('updatePipeline', () => {
+    const data = {
+      id: pipelineID,
+      params: {
+        name: pipelineName,
+        rules: {
+          [topicID]: [],
+        },
+        workerClusterName: wkName,
+      },
+    };
+    cy.updatePipeline(data).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys(
+        'id',
+        'name',
+        'workerClusterName',
+        'objects',
+        'rules',
+      );
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.name).to.be.a('string');
+      expect(data.result.workerClusterName).to.be.a('string');
+      expect(data.result.objects).to.be.a('array');
+      expect(data.result.rules).to.be.a('object');
+      expect(data.result.objects[0].id).to.eq(topicID);
+      expect(data.result.objects[0].kind).to.eq('topic');
+      expect(data.result.objects[0].name).to.eq(topicID);
+    });
+  });
+  it('deletePipeline', () => {
+    cy.testDeletePipeline(pipelineID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+    });
+  });
+});
+
+describe('Connector Api test', () => {
+  it('createConnector', () => {
+    const data = {
+      className: 'com.island.ohara.connector.ftp.FtpSource',
+      configs: {},
+      'connector.name': 'Untitled source',
+      name: 'Untitled source',
+      numberOfTasks: 1,
+      schema: [],
+      topics: [],
+      workerClusterName: wkName,
+    };
+    cy.createConnector(data).then(res => {
+      const data = res.data;
+      connectorID = data.result.id;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys('id', 'settings', 'state');
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.state).to.be.a('null');
+      expect(data.result.settings).to.include.keys(
+        'connector.class',
+        'connector.name',
+        'name',
+        'tasks.max',
+        'workerClusterName',
+      );
+      expect(data.result.settings).to.be.a('object');
+      expect(data.result.settings['connector.class']).to.be.a('string');
+      expect(data.result.settings['connector.name']).to.be.a('string');
+      expect(data.result.settings['tasks.max']).to.be.a('number');
+      expect(data.result.settings.name).to.be.a('string');
+      expect(data.result.settings.workerClusterName).to.be.a('string');
+    });
+  });
+  it('fetchConnector', () => {
+    cy.fetchConnector(connectorID).then(res => {
+      const data = res.data;
+      connectorID = data.result.id;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys('id', 'settings', 'state');
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.state).to.be.a('null');
+      expect(data.result.settings).to.include.keys(
+        'connector.class',
+        'connector.name',
+        'name',
+        'tasks.max',
+        'workerClusterName',
+      );
+      expect(data.result.settings).to.be.a('object');
+      expect(data.result.settings['connector.class']).to.be.a('string');
+      expect(data.result.settings['connector.name']).to.be.a('string');
+      expect(data.result.settings['tasks.max']).to.be.a('number');
+      expect(data.result.settings.name).to.be.a('string');
+      expect(data.result.settings.workerClusterName).to.be.a('string');
+    });
+  });
+  it('updateConnector', () => {
+    const data = {
+      id: connectorID,
+      params: {
+        author: 'root',
+        columns: [
+          { dataType: 'STRING', name: 'test', newName: 'test', order: 1 },
+        ],
+        'connector.class': 'com.island.ohara.connector.ftp.FtpSource',
+        'connector.name': 'Untitled source',
+        'ftp.completed.folder': 'test',
+        'ftp.encode': 'UTF-8',
+        'ftp.error.folder': 'test',
+        'ftp.hostname': 'test',
+        'ftp.input.folder': 'test',
+        'ftp.port': 20,
+        'ftp.user.name': 'test',
+        'ftp.user.password': 'test',
+        kind: 'source',
+        revision: '1e7da9544e6aa7ad2f9f2792ed8daf5380783727',
+        'tasks.max': 1,
+        topics: [topicID],
+        version: '0.6-SNAPSHOT',
+        workerClusterName: wkName,
+      },
+    };
+    cy.updateConnector(data).then(res => {
+      const data = res.data;
+      cy.log(data);
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys('id', 'settings', 'state');
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.state).to.be.a('null');
+      expect(data.result.settings).to.be.a('object');
+      expect(data.result.settings).to.include.keys(
+        'author',
+        'columns',
+        'connector.class',
+        'connector.name',
+        'ftp.completed.folder',
+        'ftp.encode',
+        'ftp.error.folder',
+        'ftp.hostname',
+        'ftp.input.folder',
+        'ftp.port',
+        'ftp.user.name',
+        'ftp.user.password',
+        'kind',
+        'revision',
+        'tasks.max',
+        'topics',
+        'version',
+        'workerClusterName',
+      );
+      expect(data.result.settings.author).to.be.a('string');
+      expect(data.result.settings.columns).to.be.a('array');
+      expect(data.result.settings['connector.class']).to.be.a('string');
+      expect(data.result.settings['connector.name']).to.be.a('string');
+      expect(data.result.settings['connector.class']).to.be.a('string');
+      expect(data.result.settings['ftp.completed.folder']).to.be.a('string');
+      expect(data.result.settings['ftp.encode']).to.be.a('string');
+      expect(data.result.settings['ftp.error.folder']).to.be.a('string');
+      expect(data.result.settings['ftp.hostname']).to.be.a('string');
+      expect(data.result.settings['ftp.input.folder']).to.be.a('string');
+      expect(data.result.settings['ftp.port']).to.be.a('number');
+      expect(data.result.settings['ftp.user.name']).to.be.a('string');
+      expect(data.result.settings['ftp.user.password']).to.be.a('string');
+      expect(data.result.settings.kind).to.be.a('string');
+      expect(data.result.settings.revision).to.be.a('string');
+      expect(data.result.settings['tasks.max']).to.be.a('number');
+      expect(data.result.settings.topics).to.be.a('array');
+      expect(data.result.settings.version).to.be.a('string');
+      expect(data.result.settings.workerClusterName).to.be.a('string');
+    });
+  });
+  it('startConnector', () => {
+    cy.startConnector(connectorID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys('id', 'settings', 'state');
+      expect(data.result.state).to.be.a('string');
+    });
+  });
+  it('stopConnector', () => {
+    cy.stopConnector(connectorID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys('id', 'settings', 'state');
+      expect(data.result.state).to.be.a('null');
+    });
+  });
+  it('deleteConnector', () => {
+    cy.deleteConnector(connectorID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+    });
+  });
+});
+
+describe('Streamapp Api test', () => {
+  it('uploadStreamAppJar', () => {
+    const params = {
+      wk: wkName,
+      jarName: 'ohara-streamapp.jar',
+    };
+    cy.testUploadStreamAppJar(params).then(res => {
+      const data = res.data;
+      streamAppID = data.result[0].id;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result[0]).to.include.keys('id', 'name', 'workerClusterName');
+      expect(data.result[0].id).to.be.a('string');
+      expect(data.result[0].name).to.be.a('string');
+      expect(data.result[0].workerClusterName).to.be.a('string');
+    });
+  });
+  it('fetchStreamAppJars', () => {
+    cy.fetchStreamAppJars(wkName).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result[0]).to.include.keys('id', 'name', 'workerClusterName');
+      expect(data.result[0].id).to.be.a('string');
+      expect(data.result[0].name).to.be.a('string');
+      expect(data.result[0].workerClusterName).to.be.a('string');
+    });
+  });
+  it('createProperty', () => {
+    const params = {
+      jarId: streamAppID,
+      name: 'Untitled streamApp',
+    };
+    cy.createProperty(params).then(res => {
+      const data = res.data;
+      propertyID = data.result.id;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys(
+        'id',
+        'instances',
+        'jarInfo',
+        'name',
+        'from',
+        'to',
+        'workerClusterName',
+      );
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.instances).to.be.a('number');
+      expect(data.result.name).to.be.a('string');
+      expect(data.result.from).to.be.a('array');
+      expect(data.result.to).to.be.a('array');
+      expect(data.result.workerClusterName).to.be.a('string');
+      expect(data.result.jarInfo).to.be.a('object');
+      expect(data.result.jarInfo).to.include.keys('name', 'id');
+    });
+  });
+  it('fetchProperty', () => {
+    cy.fetchProperty(propertyID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys(
+        'id',
+        'instances',
+        'jarInfo',
+        'name',
+        'from',
+        'to',
+        'workerClusterName',
+      );
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.instances).to.be.a('number');
+      expect(data.result.name).to.be.a('string');
+      expect(data.result.from).to.be.a('array');
+      expect(data.result.to).to.be.a('array');
+      expect(data.result.workerClusterName).to.be.a('string');
+      expect(data.result.jarInfo).to.be.a('object');
+      expect(data.result.jarInfo).to.include.keys('name', 'id');
+    });
+  });
+  it('updateProperty', () => {
+    const params = {
+      id: propertyID,
+      jarId: streamAppID,
+      name: 'test',
+      from: [topicID],
+      to: [],
+      instances: 1,
+    };
+    cy.updateProperty(params).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys(
+        'id',
+        'instances',
+        'jarInfo',
+        'name',
+        'from',
+        'to',
+        'workerClusterName',
+      );
+      expect(data.result.id).to.be.a('string');
+      expect(data.result.instances).to.be.a('number');
+      expect(data.result.name).to.be.a('string');
+      expect(data.result.from).to.be.a('array');
+      expect(data.result.to).to.be.a('array');
+      expect(data.result.workerClusterName).to.be.a('string');
+      expect(data.result.jarInfo).to.be.a('object');
+      expect(data.result.jarInfo).to.include.keys('name', 'id');
+      expect(data.result.name).to.eq('test');
+      expect(data.result.from[0]).to.eq(topicID);
+    });
+  });
+  it('stopStreamApp', () => {
+    cy.stopStreamApp(propertyID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+    });
+  });
+  it('deleteProperty', () => {
+    cy.stopStreamApp(propertyID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+    });
+  });
+  it('deleteStreamAppJar', () => {
+    cy.deleteStreamAppJar(streamAppID).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+    });
+  });
+});
+
+describe('Log Api test', () => {
+  it('fetchLogs', () => {
+    cy.fetchLogs('workers', wkName).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result).to.include.keys('name', 'logs');
+      expect(data.result.name).to.be.a('string');
+      expect(data.result.logs).to.be.a('array');
+      expect(data.result.logs[0]).to.include.keys('name', 'value');
+      expect(data.result.logs[0].name).to.be.a('string');
+      expect(data.result.logs[0].value).to.be.a('string');
+    });
+  });
+});
+
+describe('Validate Api test', () => {
+  it('validateConnector', () => {
+    const params = {
+      author: 'root',
+      columns: [
+        { dataType: 'STRING', name: 'test', newName: 'test', order: 1 },
+      ],
+      'connector.class': 'com.island.ohara.connector.ftp.FtpSource',
+      'connector.name': 'Untitled source',
+      'ftp.completed.folder': 'test',
+      'ftp.encode': 'UTF-8',
+      'ftp.error.folder': 'test',
+      'ftp.hostname': 'test',
+      'ftp.input.folder': 'test',
+      'ftp.port': 20,
+      'ftp.user.name': 'test',
+      'ftp.user.password': 'test',
+      kind: 'source',
+      revision: '1e7da9544e6aa7ad2f9f2792ed8daf5380783727',
+      'tasks.max': 1,
+      topics: [topicID],
+      version: '0.6-SNAPSHOT',
+      workerClusterName: wkName,
+    };
+    cy.validateConnector(params).then(res => {
+      const data = res.data;
+      expect(data.isSuccess).to.eq(true);
+      expect(data.result.errorCount).to.eq(0);
+      expect(data.result).to.include.keys('errorCount', 'settings');
+      expect(data.result.settings).to.be.a('array');
     });
   });
 });
