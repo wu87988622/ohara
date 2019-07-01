9568e28f33f2cf1d8ee1533b9836ba2267f31727
wu87988622 <wu87988622@gmail.com>
2019-07-01 16:55:16 +0800

    fix modal streamApp

--- ohara-manager/client/src/components/pages/PipelinePage/PipelineToolbar/PipelineNewStream.js
+++ ohara-manager/client/src/components/pages/PipelinePage/PipelineToolbar/PipelineNewStream.js
@@ -89,8 +89,9 @@ class PipelineNewStream extends React.Component {
       group: get(jars, '[0].group', null),
       name: get(jars, '[0].name', null),
     };
-    updateAddBtnStatus(activeJar);
-
+    if (activeJar.group == null && activeJar.name == null) {
+      updateAddBtnStatus(null);
+    }
     if (!isNull(jars)) {
       this.setState({ jars, activeJar });
     }
