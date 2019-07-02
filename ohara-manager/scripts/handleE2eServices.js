c11d04f2cc76c0d7e33517c3049627dbee43160b
RobertYE <wu87988622@gmail.com>
2019-07-02 09:50:32 +0800

    #1666 Fix create e2e services wait containers failed (#1668)

--- ohara-manager/scripts/handleE2eServices.js
+++ ohara-manager/scripts/handleE2eServices.js
@@ -17,6 +17,7 @@
 const fs = require('fs');
 const axios = require('axios');
 const chalk = require('chalk');
+const { get, isNull } = require('lodash');
 
 const commonUtils = require('../utils/commonUtils');
 
@@ -117,11 +118,8 @@ const waitForCreateService = async (baseUrl, api, name) => {
 
 const waitContainersCreate = async (baseUrl, name) => {
   const res = await axios.get(`${baseUrl}/containers/${name}`);
-  if (
-    typeof res === 'undefined' ||
-    res.data[0].containers.length == 0 ||
-    res.data[0].containers[0].state !== 'RUNNING'
-  ) {
+  const containers = get(res, 'data[0].containers[0].state', null);
+  if (isNull(containers)) {
     await commonUtils.sleep(1000);
     await waitContainersCreate(baseUrl, name);
   }
