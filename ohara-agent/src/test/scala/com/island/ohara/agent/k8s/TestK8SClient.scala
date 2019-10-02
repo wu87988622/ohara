c335d13e78514b28e10110bc7bb43ed70a9006ad
Joshua <joshua@is-land.com.tw>
2019-10-02 13:39:29 +0800

    #2865 Update master branch's SNAPSHOT version to 0.9.0-SNAPSHOT (#2866)

--- ohara-agent/src/test/scala/com/island/ohara/agent/k8s/TestK8SClient.scala
+++ ohara-agent/src/test/scala/com/island/ohara/agent/k8s/TestK8SClient.scala
@@ -439,7 +439,7 @@ class TestK8SClient extends OharaTest with Matchers {
                         |        "containers": [
                         |          {
                         |            "name": "ohara",
-                        |            "image": "oharastream/broker:0.8.0-SNAPSHOT",
+                        |            "image": "oharastream/broker:0.9.0-SNAPSHOT",
                         |            "ports": [
                         |              {
                         |                "hostPort": 43507,
