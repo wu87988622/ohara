af4a40f26a12afb43c46d0f67fdb3c615d6d07b8
RobertYE <wu87988622@gmail.com>
2020-02-12 10:28:13 +0800

    #3983 [Manager] Reset Workspace (#3985)

--- ohara-manager/client/src/components/Workspace/Edit/MoreActions.js
+++ ohara-manager/client/src/components/Workspace/Edit/MoreActions.js
@@ -19,9 +19,30 @@ import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreIcon from '@material-ui/icons/MoreVert';
+import Typography from '@material-ui/core/Typography';
+
+import * as context from 'context';
+import { KIND } from 'const';
+import { Dialog } from 'components/common/Dialog';
+import { Progress } from 'components/common/Progress';
 
 function MoreActions() {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [isOpen, setIsOpne] = React.useState(false);
+  const [isResetting, setIsResetting] = React.useState(false);
+  const [activeStep, setActiveStep] = React.useState(0);
+  const {
+    currentPipeline,
+    currentZookeeper,
+    currentBroker,
+    currentWorker,
+  } = context.useWorkspace();
+  const { stopConnector, startConnector } = context.useConnectorActions();
+  const { stopTopic, startTopic } = context.useTopicActions();
+  const { stopStream, startStream } = context.useStreamActions();
+  const { stopWorker, startWorker } = context.useWorkerActions();
+  const { stopBroker, startBroker } = context.useBrokerActions();
+  const { stopZookeeper, startZookeeper } = context.useZookeeperActions();
 
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
@@ -29,6 +50,67 @@ function MoreActions() {
 
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const handleDialogClose = () => {
+    setIsOpne(false);
+  };
+
+  const steps = [
+    'Stop Pipeline',
+    'Stop Workspace',
+    'Start Workspace',
+    'Start Pipeline',
+  ];
+
+  const reset = async () => {
+    setIsResetting(true);
+    const objects = currentPipeline.objects;
+    const connectors = objects
+      .filter(
+        object => object.kind === KIND.source || object.kind === KIND.sink,
+      )
+      .filter(object => object.state === 'RUNNING');
+    const topics = objects
+      .filter(object => object.kind === KIND.topic)
+      .filter(object => object.state === 'RUNNING');
+    const streams = objects
+      .filter(object => object.kind === KIND.stream)
+      .filter(object => object.state === 'RUNNING');
+
+    for (const connector of connectors) {
+      await stopConnector(connector.name);
+    }
+    for (const stream of streams) {
+      await stopStream(stream.name);
+    }
+    for (const topic of topics) {
+      await stopTopic(topic.name);
+    }
+
+    setActiveStep(1);
+    await stopWorker(currentWorker.name);
+    await stopBroker(currentBroker.name);
+    await stopZookeeper(currentZookeeper.name);
+    setActiveStep(2);
+    await startZookeeper(currentZookeeper.name);
+    await startBroker(currentBroker.name);
+    await startWorker(currentWorker.name);
+    setActiveStep(3);
+
+    for (const topic of topics) {
+      await startTopic(topic.name);
+    }
+    for (const connector of connectors) {
+      await startConnector(connector.name);
+    }
+    for (const stream of streams) {
+      await startStream(stream.name);
+    }
+
+    setActiveStep(4);
+    setIsOpne(false);
+    handleClose();
   };
   return (
     <>
@@ -47,9 +129,33 @@ function MoreActions() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
-        <MenuItem onClick={handleClose}>ADD PLUGINS</MenuItem>
-        <MenuItem onClick={handleClose}>ADD NODES</MenuItem>
+        <MenuItem onClick={() => setIsOpne(true)}>RESET WORKSPACE</MenuItem>
+        {/* Feature is disabled because it's not implemented in 0.9 */
+        false && <MenuItem onClick={handleClose}>ADD PLUGINS</MenuItem>}
+        {/* Feature is disabled because it's not implemented in 0.9 */
+        false && <MenuItem onClick={handleClose}>ADD NODES</MenuItem>}
       </Menu>
+      <Dialog
+        open={isOpen}
+        title={'Reset Workspace?'}
+        children={
+          <Typography>
+            Please be reminded that this reset will restart all services that
+            are currently running. There are changes that the data being
+            processed could be lost and there’s no way to undo this action.
+          </Typography>
+        }
+        confirmText={'RESET'}
+        handleClose={handleDialogClose}
+        handleConfirm={reset}
+      />
+      <Progress
+        createTitle={'Resetting'}
+        open={isResetting}
+        steps={steps}
+        activeStep={activeStep}
+        maxWidth="md"
+      />
     </>
   );
 }
