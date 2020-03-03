4b7e17509e1993097308e7ab840520ef8faba13f
Joshua <joshua@is-land.com.tw>
2020-03-03 11:03:43 +0800

    #4209 [Manager] Prevent Paper's dragging event interfering with Toolbox (#4211)

--- ohara-manager/client/src/components/Pipeline/Paper/Paper.js
+++ ohara-manager/client/src/components/Pipeline/Paper/Paper.js
@@ -145,6 +145,7 @@ const Paper = React.forwardRef((props, ref) => {
     });
 
     paper.on('blank:pointermove', event => {
+      $('.toolbox').css('pointer-events', 'none');
       if (dragStartPosition) {
         paper.translate(
           event.offsetX - dragStartPosition.x,
@@ -155,6 +156,7 @@ const Paper = React.forwardRef((props, ref) => {
 
     paper.on('blank:pointerup', () => {
       setDragStartPosition(null);
+      $('.toolbox').css('pointer-events', 'auto');
     });
 
     paper.on('blank:mouseover', () => {
