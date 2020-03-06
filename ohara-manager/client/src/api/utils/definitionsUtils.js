350a324ceebc954c102bcc19ed01a8046821ddfa
wu87988622 <wu87988622@gmail.com>
2020-03-06 11:55:24 +0800

    #4190 [Manager] Hide the group info from Property view

--- ohara-manager/client/src/api/utils/definitionsUtils.js
+++ ohara-manager/client/src/api/utils/definitionsUtils.js
@@ -103,20 +103,40 @@ const getTypeWithValueType = key => {
 
 export const getDefinition = params => {
   const settingDefinitions = get(params, 'settingDefinitions', []);
+  const isStream =
+    get(
+      settingDefinitions.find(def => def.key === 'imageName'),
+      'defaultValue',
+      '',
+    ).indexOf('oharastream/stream:') > -1;
   const newDefs = {};
+  const internalList = [
+    'group',
+    'tags',
+    'zookeeperClusterKey',
+    'brokerClusterKey',
+    'workerClusterKey',
+  ];
   settingDefinitions.forEach(def => {
     const keys = Object.keys(def);
     keys
-      .filter(key => key === 'key' || key === 'group')
+      .filter(key => key === 'key')
       .forEach(key => {
         // The value `.` doesn't work very well with final form
         if (def[key].includes('.')) {
           def[key] = def[key].replace(/\./g, '__');
         }
 
-        // Group is hidden from our UI
-        if (def[key] === 'group') {
+        // Some keys are hidden from our UI
+        if (internalList.includes(def[key])) {
           def.internal = true;
+        }
+
+        // The value type of the stream's settingdefinition is BINDING_PORT.
+        // We must change it to REMOTE_PORT.
+        // Because BINDING_PORT is in the UI, We will associate it with worker free port, but in fact they are not.
+        if (isStream && def.valueType === 'BINDING_PORT') {
+          def.valueType = 'REMOTE_PORT';
         }
       });
     newDefs[def.key] = def;
