b19831cf1ec8b722bf1d50dc1b53acd432d4d1a9
Chih-Chiang Yeh <eechih@gmail.com>
2020-03-20 10:23:06 +0800

    #4384 [Manager] Introduce Redux-Form instead of Final-Form (#4399)

--- ohara-manager/client/src/store/reducers/index.js
+++ ohara-manager/client/src/store/reducers/index.js
@@ -15,10 +15,12 @@
  */
 
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import entities from './entities';
 import ui from './ui';
 
 export default combineReducers({
   entities,
+  form: formReducer,
   ui,
 });
