1b77c2d998f420c29fb5eea9caeaf8ee39f7890b
Joshua <joshua@is-land.com.tw>
2020-05-08 22:46:49 +0800

    #4764 [Manager] Jumping between settings pages are very slow (#4769)

---
+++ ohara-manager/client/src/components/Workspace/Settings/SectionComponent.js
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2019 is-land
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Box from '@material-ui/core/Box';
+
+import { Dialog } from 'components/common/Dialog';
+import { SETTINGS_COMPONENT_TYPES } from 'const';
+
+const SectionComponent = props => {
+  const { sections, selectedComponent, handleClose } = props;
+
+  if (!selectedComponent) return null;
+
+  return (
+    <>
+      {sections.map(section =>
+        section.components.map(component => {
+          const { title } = component;
+          if (title !== selectedComponent.name) return null;
+          return (
+            <Box key={title}>
+              {componentRenderer({
+                component,
+                selectedComponent,
+                handleClose,
+              })}
+            </Box>
+          );
+        }),
+      )}
+    </>
+  );
+};
+
+function componentRenderer({ component, selectedComponent, handleClose }) {
+  const { title, type, componentProps } = component;
+
+  switch (type) {
+    case SETTINGS_COMPONENT_TYPES.PAGE:
+      return (
+        <div className="section-page">
+          <div className="section-page-header">
+            <IconButton edge="start" color="inherit" onClick={handleClose}>
+              <KeyboardBackspaceIcon />
+            </IconButton>
+            <Typography variant="h4" component="h2">
+              {title}
+            </Typography>
+          </div>
+
+          <div className="section-page-content">{componentProps?.children}</div>
+        </div>
+      );
+
+    case SETTINGS_COMPONENT_TYPES.DIALOG:
+      return (
+        <Dialog
+          open={title === selectedComponent.name}
+          onClose={handleClose}
+          {...componentProps}
+        />
+      );
+
+    case SETTINGS_COMPONENT_TYPES.CUSTOMIZED:
+      return null;
+
+    default:
+      throw new Error(
+        `Couldn't render the component with an unknown type of ${type}`,
+      );
+  }
+}
+
+SectionComponent.propTypes = {
+  sections: PropTypes.arrayOf(
+    PropTypes.shape({
+      heading: PropTypes.string.isRequired,
+      ref: PropTypes.object,
+      components: PropTypes.arrayOf(
+        PropTypes.shape({
+          icon: PropTypes.node,
+          text: PropTypes.string,
+        }),
+      ),
+    }),
+  ).isRequired,
+  handleClose: PropTypes.func.isRequired,
+  selectedComponent: PropTypes.shape({
+    name: PropTypes.string,
+  }),
+};
+
+export default React.memo(SectionComponent);
---
+++ ohara-manager/client/src/components/Workspace/Settings/SectionList.js
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2019 is-land
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Badge from '@material-ui/core/Badge';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import IconButton from '@material-ui/core/IconButton';
+
+import { SETTINGS_COMPONENT_TYPES } from 'const';
+
+const SectionList = props => {
+  const { list, handleChange, sectionHeading, sectionRef } = props;
+
+  return (
+    <List component="ul">
+      {list.map(item => {
+        const {
+          title,
+          icon,
+          type,
+          subTitle,
+          badge,
+          componentProps = {},
+        } = item;
+        const { children, handleClick } = componentProps;
+
+        const onClick = (event, title) => {
+          switch (type) {
+            case SETTINGS_COMPONENT_TYPES.PAGE:
+            case SETTINGS_COMPONENT_TYPES.DIALOG:
+              return handleChange({
+                type,
+                name: title,
+                ref: sectionRef,
+                heading: sectionHeading,
+              });
+
+            case SETTINGS_COMPONENT_TYPES.CUSTOMIZED:
+              return (
+                typeof handleClick === 'function' && handleClick(event, title)
+              );
+
+            default:
+              throw new Error(`Unknown type of ${type}`);
+          }
+        };
+
+        return (
+          <ListItem key={title} onClick={event => onClick(event, title)}>
+            <Badge component="div" badgeContent={badge?.count}>
+              {icon && <ListItemIcon>{icon}</ListItemIcon>}
+              <ListItemText primary={title} secondary={subTitle} />
+
+              {type === SETTINGS_COMPONENT_TYPES.PAGE && (
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    size="small"
+                    onClick={event => onClick(event, title)}
+                  >
+                    <ArrowRightIcon fontSize="small" />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              )}
+
+              {type === SETTINGS_COMPONENT_TYPES.CUSTOMIZED && <>{children}</>}
+            </Badge>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
+
+SectionList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      icon: PropTypes.node,
+      text: PropTypes.string,
+    }),
+  ),
+  handleChange: PropTypes.func.isRequired,
+  selectedComponent: PropTypes.shape({
+    name: PropTypes.string,
+  }),
+  sectionHeading: PropTypes.string.isRequired,
+  sectionRef: PropTypes.object,
+};
+
+export default React.memo(SectionList);
