83f00b91fcd7aedf7d6faf6124506224d795abbd
saivirtue <sam@is-land.com.tw>
2020-06-02 15:21:34 +0800

    #5111 Add some simple filter logic for the testing purpose stream jar (#5137)

--- ohara-it/src/main/scala/oharastream/ohara/it/stream/DumbStream.scala
+++ ohara-it/src/main/scala/oharastream/ohara/it/stream/DumbStream.scala
@@ -15,17 +15,59 @@
  */
 
 package oharastream.ohara.it.stream
+import java.util
+
 import oharastream.ohara.common.data.Row
+import oharastream.ohara.common.setting.SettingDef
 import oharastream.ohara.stream.config.StreamSetting
 import oharastream.ohara.stream.{OStream, Stream}
+import scala.jdk.CollectionConverters._
 
 /**
   * This is a sample stream that will do nothing but write data to output topic
   * It is not placed at test scope since we need jar when tests manually.
   */
 class DumbStream extends Stream {
+  private val FILTER_HEADER_KEY = "FILTER_HEADER_KEY"
+  private val FILTER_VALUE_KEY  = "FILTER_VALUE_KEY"
+
+  override protected def customSettingDefinitions(): util.Map[String, SettingDef] =
+    Map(
+      FILTER_VALUE_KEY -> SettingDef
+        .builder()
+        .key(FILTER_HEADER_KEY)
+        .displayName("header name to be filtered")
+        .optional(SettingDef.Type.STRING)
+        .build(),
+      FILTER_VALUE_KEY -> SettingDef
+        .builder()
+        .key(FILTER_VALUE_KEY)
+        .displayName("column value to be filtered")
+        .optional(SettingDef.Type.STRING)
+        .build()
+    ).asJava
+
   override def start(ostream: OStream[Row], configs: StreamSetting): Unit = {
-    // do nothing but only start stream and write exactly data to output topic
-    ostream.start()
+    ostream
+      .filter(row => {
+        val headerName = configs.string(FILTER_HEADER_KEY)
+        val value      = configs.string(FILTER_VALUE_KEY)
+
+        if (headerName.isPresent) {
+          if (value.isPresent) {
+            row.names().contains(headerName.get()) && row
+              .cell(headerName.get())
+              .value()
+              .toString
+              .equalsIgnoreCase(value.get())
+          } else {
+            // if no value defined, we return rows which contain the header
+            row.names().contains(headerName.get())
+          }
+        } else
+          // if no header and value defined, we return all rows
+          true
+      })
+      .start()
   }
 }
